a3a1db75524705bac36c3fb47ff1b651f6bda249
Aleksei Emshanov <aleksei_emshanov@epam.com>
2022-10-10 23:06:58 +0500

    implement createUser method

--- app/index.ts
+++ app/index.ts
@@ -9,34 +9,49 @@ const port = 3000;
 const storage: User[] = [];
 
 const defaultUser: User = {
-    id: uuidv4(),
-    login: 'abc',
-    password: 'password',
-    age: 38,
-    isDeleted: false
-}
+  id: uuidv4(),
+  login: 'abc',
+  password: 'password',
+  age: 38,
+  isDeleted: false
+};
 
 storage.push(defaultUser);
 
+app.use(express.json()); //Body parser for requests
+
 app.get('/', (req, res) => {
-    res.send('Welcome to the test server!');
+  res.send('Welcome to the test server!');
 });
 
 app.get('/users', (req, res) => {
-    res.json(storage)
+  res.json(storage)
 });
 
 app.get('/users/:id', (req, res) => {
-    let requestedUser = _.find(storage, {id: req.params.id})
+  let requestedUser = _.find(storage, {id: req.params.id})
 
-    if (!!requestedUser) {
-        res.json(requestedUser);
-    } else {
-        res.status(404)
-            .json({message: `User with id ${req.params.id} not found`})
-    }
+  if (requestedUser) {
+    res.json(requestedUser);
+  } else {
+    res.status(404)
+      .json({message: `User with id ${req.params.id} not found`})
+  }
+});
+
+app.post('/createUser', (req, res) => {
+  const createdUser = req.body;
+
+  if ( !_.isEmpty(req.body)) {
+    storage.push(createdUser);
+    res.status(200)
+      .json({message: "User was successfully created!"})
+  } else {
+    res.status(400)
+      .json({message: "User entity couldn't be empty!"})
+  }
 });
 
 app.listen(port, () => {
-    console.log(`The application is running on ${port}`);
-})
+  console.log(`The application is running on ${port}`);
+});
