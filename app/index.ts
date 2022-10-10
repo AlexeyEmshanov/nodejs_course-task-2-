c25f04725db15578a3ddb9a0ceddd9fc9036d0f7
Aleksei Emshanov <aleksei_emshanov@epam.com>
2022-10-10 22:16:09 +0500

    implement basic get routes for user collection & user with specific ID

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,41 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+import User from "../types/user_type";
+import _ from 'lodash';
 
 const app = express();
 const port = 3000;
 
+const storage: User[] = [];
+
+const defaultUser: User = {
+    id: uuidv4(),
+    login: 'abc',
+    password: 'password',
+    age: 38,
+    isDeleted: false
+}
+
+storage.push(defaultUser);
+
 app.get('/', (req, res) => {
-    res.send('Hello my friend!!!');
-})
+    res.send('Welcome to the test server!');
+});
+
+app.get('/users', (req, res) => {
+    res.json(storage)
+});
+
+app.get('/users/:id', (req, res) => {
+    let requestedUser = _.find(storage, {id: req.params.id})
+
+    if (!!requestedUser) {
+        res.json(requestedUser);
+    } else {
+        res.status(404)
+            .json({message: `User with id ${req.params.id} not found`})
+    }
+});
 
 app.listen(port, () => {
     console.log(`The application is running on ${port}`);
