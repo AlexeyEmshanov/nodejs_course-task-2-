9b38afd349d77e56090df133cc8b2ff7fb1ce6a7
Aleksei Emshanov <aleksei_emshanov@epam.com>
2023-01-30 23:49:11 +0500

    implement many-to-many relations table

--- data-access/data-access.ts
+++ data-access/data-access.ts
@@ -1,9 +1,5 @@
 import {DataTypes, Sequelize, TEXT} from "sequelize";
-import fs from "fs";
 import {createValidator} from "express-joi-validation";
-
-const sqlScriptForUsers = fs.readFileSync('./scripts/users.sql').toString();
-const sqlScriptForGroups = fs.readFileSync('./scripts/groups.sql').toString();
 
 //DB Connection
 // const sequelize = new Sequelize('postgres://postgres:password1@localhost:5432/postgres', { dialect: "postgres" });
@@ -24,19 +20,12 @@ try {
   console.error('Unable to connect to the database', err);
 }
 
-sequelize.query(sqlScriptForUsers)
-  .then( ()=> console.log('SQL script from file for mock users has been read'))
-  .catch( (err) => console.log('ERROR->> Some problems with reading SQL script from file for mock users', err));
-
-sequelize.query(sqlScriptForGroups)
-  .then( ()=> console.log('SQL script from file for mock groups has been read'))
-  .catch( (err) => console.log('ERROR->> Some problems with reading SQL script from file for mock groups', err));
-
 //Models
-const User = sequelize.define('User', {
+const User = sequelize.define('Users', {
     id: {
       type : DataTypes.UUID,
       primaryKey: true,
+      unique: true,
       defaultValue: DataTypes.UUIDV4
     },
     login: {
@@ -54,16 +43,17 @@ const User = sequelize.define('User', {
     }
   },
   {
-    tableName: 'users',
+    tableName: 'Users',
     timestamps: false,
   }
 );
 
-const Group = sequelize.define('Group', {
+const Group = sequelize.define('Groups', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
-      defaultValue: DataTypes.UUIDV4
+      defaultValue: DataTypes.UUIDV4,
+      unique: true
     },
     name: {
       type: DataTypes.TEXT
@@ -73,21 +63,44 @@ const Group = sequelize.define('Group', {
     }
   },
   {
-    tableName: 'groups',
+    tableName: 'Groups',
     timestamps: false,
   }
 );
 
-User.sync().then(() => {
-  console.log('User model synchronizing completed')
-});
+// Many-To-Many
+const UserGroups = sequelize.define('UserGroups', {
+  UserId: {
+    type: DataTypes.UUID,
+    references: {
+      model: User,
+      key: 'id'
+    },
+  },
+  GroupId: {
+    type: DataTypes.UUID,
+    references: {
+      model: Group,
+      key: 'id'
+    },
+  },
 
-Group.sync().then(() => {
-  console.log('Group model synchronizing completed')
-});
+  }, {
+    tableName: 'UserGroups',
+    timestamps: false
+})
+
+//Default values for many-to-many tables for onDelete, onUpdate properties - CASCADE, so we can get rid of it
+User.belongsToMany(Group, {through: 'UserGroups', onDelete: 'CASCADE' });
+Group.belongsToMany(User, {through: 'UserGroups', onDelete: 'CASCADE' });
+
+sequelize.sync().then(() => {
+  console.log('All models synchronizing completed')
+
+
+})
 
 //Validation
 const validator = createValidator();
 
-export { User, Group, validator };
-
+export { User, Group, UserGroups, validator, sequelize };
